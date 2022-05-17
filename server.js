8b06b69b6e6a4d7a99d37a2571e560eb8ae34b7a
VladyslavYermolenko <104897591+VladyslavYermolenko@users.noreply.github.com>
2022-05-17 16:34:37 +0300

    Code improvement.

--- server.js
+++ server.js
@@ -1,9 +1,43 @@
 const app = require('./app')
 
+const hostname = 'localhost';
 const port = 3000;
-app.listen(port, (error) => {
-    error ? console.log(error) : console.log(`Server listens http://localhost:${port}`);
+app.listen(port, hostname, (error) => {
+    error ? console.log(error) : console.log(`Server listens http://${hostname}:${port}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //////////////////////////////////////////////////////////////////////
 //                     Test commands (lists)                        //
